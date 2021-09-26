12a2d095cf9c9a4c2e4137452c6a8397cddcbd9c
AmieLauren <64654752+AmieLauren@users.noreply.github.com>
2021-09-26 11:21:28 -0500

    root config updated; added getter/setters for state sharing

--- src/built-root-config.ts
+++ src/built-root-config.ts
@@ -1,10 +1,37 @@
 import { registerApplication, start } from "single-spa";
 
+//issue is that when apps unmount, data lost- so handling this in root config 
+//for the form info to pass between apps/micro-frontends
+let loanAppData = {};
+
+//setter
+const setLoanAppData = (data) => {
+  loanAppData = data;
+}
+
+const getLoanAppData = () => {
+  return loanAppData;
+}
+//registering each piece with single spa 
 //convention uses '@' then the org name for name(package.json has this convention there too.)
 registerApplication({
   name: "@built/loan-application",
   app: () => System.import("@built/loan-application"),
-  activeWhen: ["/loan-application"]
+  activeWhen: ["/loan-application"],
+  customProps: {
+    setLoanAppData,
+    getLoanAppData
+  }
+});
+
+registerApplication({
+  name: "@built/success",
+  app: () => System.import("@built/success"),
+  activeWhen: ["/success"],
+  customProps: {
+    setLoanAppData,
+    getLoanAppData
+  }
 });
 
 start();
